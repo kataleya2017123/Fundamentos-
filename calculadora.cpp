326c06b25cb1fef1bb2eb6ba5983b9c08c309097
kataleya2017123 <janeth.altafuya.mideros@utelvt.edu.ec>
2022-09-23 05:05:40 +0000

    modificacion de programa con cambio de variable calculadora.cpp

--- calculadora.cpp
+++ calculadora.cpp
@@ -1,19 +1,20 @@
+//modificado por janeth altafuya mideros
 #include<iostream>
 using namespace std;
 
 int main()
 {
-	float x,y,s,m,d,r;
-	cout<<"Ingrese en valor de x=:";cin>>x;
-	cout<<"Ingrese en valor de y=:";cin>>y;
-        s=x+y;
-	m=x*y;
-	d=x/y;
-	r=x-y;
-	cout<<"Las suma de "<<x<<" + "<<y<<" = "<<s<<endl;
-	cout<<"Las multiplicacion de "<<x<<" * "<<y<<" = "<<m<<endl;
-	cout<<"Las division de "<<x<<" / "<<y<<" = "<<d<<endl;
-	cout<<"Las resta de "<<x<<" - "<<y<<" = "<<r<<endl;
+	float j,a,s,m,d,r;
+	cout<<"Ingrese en valor de j=:";cin>>j;
+	cout<<"Ingrese en valor de a=:";cin>>a;
+        s=j+a;
+	m=j*a;
+	d=j/a;
+	r=j-a;
+	cout<<"Las suma de "<<j<<" + "<<a<<" = "<<s<<endl;
+	cout<<"Las multiplicacion de "<<j<<" * "<<a<<" = "<<m<<endl;
+	cout<<"Las division de "<<j<<" / "<<a<<" = "<<d<<endl;
+	cout<<"Las resta de "<<j<<" - "<<a<<" = "<<r<<endl;
 	return 0 ;
 }
 
