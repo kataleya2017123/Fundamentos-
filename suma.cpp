ad7032196500d588f52564ae22c816690c01adfe
kataleya2017123 <janeth.altafuya.mideros@utelvt.edu.ec>
2022-09-23 04:27:31 +0000

    realizacion de programa suma.cpp

---
+++ suma.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main ()
+{	float x,y,z;
+        cout<< "ingrese dos numerosseparados de espacio x y:";
+	cin>>x>>y;
+	z=x+y;
+	cout<<"El resultado es: ";
+	cout<<z<<endl;
+	return 0;
+}
